8c6e563f327ed54e5d8030e0c84c02f3917833e9
ingvcass <113540264+ingvcass@users.noreply.github.com>
2022-09-21 16:50:58 +0300

    Add files via upload

---
+++ 20th.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print("     " + ".............           @@@@@    @@@@@                      ......")
+print("       " + "...........          @     @  @     @                    .......")
+print("        " + "..........             @@@   @     @                   ........")
+print("         " + ".........           @@      @     @                  .........")
+print("          " + "........          @@@@@@@   @@@@@  th              ........")
+print("           " + ".......        -----------------------           ........")
+print("            " + ".....          C  E  N  T  U  R  Y            .......")
+print("             " + "....        -----------------------         .....")
+print("              " + "...        @@@@@ @@@@@ @   @ @@@@@        ...")
+print("                " + "==          @   @      @ @    @          ==")
+print("              " + "__||__        @   @@@@    @     @        __||__")
+print("             " + "|      |       @   @      @ @    @       |      |")
+print("    " + "_________|______|_____  @   @@@@@ @   @   @  _____|______|_________")
