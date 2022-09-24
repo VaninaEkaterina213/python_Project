b454e6cff6d61cc2b3e59ffcf656517f2f237110
ingvcass <113540264+ingvcass@users.noreply.github.com>
2022-09-24 12:48:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# yo nigga
-
+print("Сколько чисел вы будите использовать?")
+colvo=int(input())
+print("введите 1 число")
+num1 = int(input())
+count=1
+while count<colvo:
+    print("введите следующее число")
+    numx=int(input())
+    print("выберите действие: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление, 5 - уравнение")
+    action=input()
+    if action == 1:
+        if count ==1:
+            sum = num1 + numx
+        else:
+            sum = sum + numx
+    elif
