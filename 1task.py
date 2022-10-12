893d0c344c116953b2d73d5b951526482a264417
ingvcass <113540264+ingvcass@users.noreply.github.com>
2022-10-12 19:22:22 +0300

    Add files via upload

---
+++ 1task.py
@@ -0,0 +1,11 @@
+def min(a,b,c):
+    if(a<b and a<c ):
+        print("Минимум: "+ str(a))
+    elif (b<a and b<c):
+        print("Минимум: "+ str(b))
+    else:
+        print("Минимум: " + str(c))
+print("Введите 3 числа")
+a = [int(input())
+     for i in range (3)]
+min(a[0], a[1], a[2])
