de19ba7dd28a0f3d805fabee7f2630dcb03f9b35
User <vanina_ekaterina_213@bk.ru>
2023-01-21 21:33:24 +0300

    ааааааа ооооооо пон

---
+++ src/Main_First.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main_First {
+    public static void main(String[] args) {
+        jopa();
+        System.out.println("Пон");
+}
+static void jopa(){
+        System.out.println("lvfmkvorfovk");
+}
+}
